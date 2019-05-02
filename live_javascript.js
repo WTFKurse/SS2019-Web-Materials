e5b4b1333d262734e68dae7883cadca2495e7dc9
Patrik Phan <patrik.phan@mailbox.tu-dresden.de>
2019-05-02 14:01:11 +0200

    Create live_javascript.js

---
+++ live_javascript.js
@@ -0,0 +1,5 @@
+var name = prompt('Was ist dein Name?');
+var age = prompt('Was ist dein Alter?');
+var education = prompt('Welche Bildungseinrichtung besuchst du?');
+
+alert("Hi " + name + "! Soso, du bist also " + age + " Jahre alt und besuchst die " + education + ". Verrückt!");
